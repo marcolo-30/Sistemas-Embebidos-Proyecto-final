7d0e261d39dcc6a3fa84875714fa5a36c5eabdac
Marco Loaiza <84221113+marcolo-30@users.noreply.github.com>
2021-06-04 13:31:53 -0500

    Update check_sensores.sh

--- check_sensores.sh
+++ check_sensores.sh
@@ -1,24 +1,24 @@
 #!/bin/bash
 
-sleep 60 
+sleep 60        #Espera mientras inicia y se configura el sistema.
 
 while true
 
 do
-    sensor=$(cat /dev/ttyAMA0 | grep GPGGA) 
+    sensor=$(cat /dev/ttyAMA0 | grep GPGGA)  #verifica si el GPS tiene conexion
     
-    if [ -z "$sensor" ]
+    if [ -z "$sensor" ]          #Verifica por primera vez
       
-    sleep 120 
+    sleep 60 
     
-    if [ -z "$sensor" ]
+    if [ -z "$sensor" ]          #verifica por segunda vez despues de dos minutos si no se encuentra el GPS reinicia la Rpi
     
     then
         sudo reboot 
     fi
     
     else
-        sleep 120 
+        sleep 120                #vuelve a realizar la verificacion cada 2 minutos. 
     fi
     
 done
